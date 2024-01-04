4176ae778cc8ef3dc175e6acd1eec5d7e6ce4184
Emmanuel Taiwo <100073682+devbird007@users.noreply.github.com>
2024-01-04 09:13:31 +0100

    recent update

--- TODO-CLI tool/todo_test.go
+++ TODO-CLI tool/todo_test.go
@@ -33,6 +33,70 @@ func TestComplete(t *testing.T) {
 	if l[0].Done {
 		t.Errorf("New task should not be completed.")
 	}
+
+	l.Complete(1)
+
+	if !l[0].Done {
+		t.Errorf("New task should be completed.")
+	}
+}
+
+// TestDelete tests the Delete method of the List type
+func TestDelete(t *testing.T) {
+	l := todo.List{}
+
+	tasks := []string{
+		"New Task 1",
+		"New Task 2",
+		"New Task 3",
+	}
+	for _, task := range tasks {
+		l.Add(task)
+	}
+
+	if l[0].task != tasks[0] {
+		t.Errorf("Expected %q, got %q instead.", tasks[0], l[0].task)
+	}
+
+	
+
+
+
+
+
+
+
+
+
+
+
+
+// TestAdd tests the Add method of the List type
+func TestAdd(t *testing.T) {
+	l := todo.List{}
+
+	taskName := "New Task"
+	l.Add(taskName)
+
+	if l[0].Task != taskName {
+		t.Errorf("Expected %q, got %q instead.", taskName, l[0].Task)
+	}
+}
+
+// TestComplete tests the Complete method of the List type
+func TestComplete(t *testing.T) {
+	l := todo.List{}
+
+	taskName := "New Task"
+	l.Add(taskName)
+
+	if l[0].Task != taskName {
+		t.Errorf("Expected %q, got %q instead.", taskName, l[0].Task)
+	}
+
+	if l[0].Done {
+		t.Errorf("New task should not be completed.")
+	}
 	l.Complete(1)
 
 	if !l[0].Done {
