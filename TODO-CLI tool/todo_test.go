6bfbf565df625a635c4c14647589d16f348564e3
Emmanuel Taiwo <100073682+devbird007@users.noreply.github.com>
2024-01-08 07:49:15 +0100

    x

--- TODO-CLI tool/todo_test.go
+++ TODO-CLI tool/todo_test.go
@@ -50,86 +50,45 @@ func TestDelete(t *testing.T) {
 		"New Task 2",
 		"New Task 3",
 	}
-	for _, task := range tasks {
-		l.Add(task)
-	}
-
-	if l[0].task != tasks[0] {
-		t.Errorf("Expected %q, got %q instead.", tasks[0], l[0].task)
-	}
-
-	
-
-
-
-
-
-
-
-
-
-
-
-
-// TestAdd tests the Add method of the List type
-func TestAdd(t *testing.T) {
-	l := todo.List{}
-
-	taskName := "New Task"
-	l.Add(taskName)
-
-	if l[0].Task != taskName {
-		t.Errorf("Expected %q, got %q instead.", taskName, l[0].Task)
-	}
-}
-
-// TestComplete tests the Complete method of the List type
-func TestComplete(t *testing.T) {
-	l := todo.List{}
-
-	taskName := "New Task"
-	l.Add(taskName)
-
-	if l[0].Task != taskName {
-		t.Errorf("Expected %q, got %q instead.", taskName, l[0].Task)
-	}
-
-	if l[0].Done {
-		t.Errorf("New task should not be completed.")
-	}
-	l.Complete(1)
-
-	if !l[0].Done {
-		t.Errorf("New task should be completed.")
-	}
-}
-
-// TestDelete tests the Delete method of the List type
-func TestDelete(t *testing.T) {
-	l := todo.List{}
-
-	tasks := []string{
-		"New Task 1",
-		"New Task 2",
-		"New Task 3",
-	}
-
 	for _, v := range tasks {
 		l.Add(v)
 	}
+
 	if l[0].Task != tasks[0] {
 		t.Errorf("Expected %q, got %q instead.", tasks[0], l[0].Task)
 	}
-
 	l.Delete(2)
 	if len(l) != 2 {
 		t.Errorf("Expected list length %d, got %d instead.", 2, len(l))
 	}
-
 	if l[1].Task != tasks[2] {
 		t.Errorf("Expected %q, got %q instead.", tasks[2], l[1].Task)
 	}
 }
+
+// TestSaveGet tests the Save and Get methods of the List type
+func TestSaveGet(t *testing.T) {
+	l1 := todo.List{}
+	l2 := todo.List{}
+
+	taskName := "New Task"
+	l1.Add(taskName)
+	
+	if l1[0].Task != taskName {
+		t.Errorf("Expected %q, got %q instead.", taskName, l1[0].Task)
+	}
+
+
+
+
+
+
+
+
+
+
+
+
 
 // TestSaveGet tests the Save and Get methods of the List type
 func TestSaveGet(t *testing.T) {
