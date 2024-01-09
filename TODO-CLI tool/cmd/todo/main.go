cae970fda36a3e1409e250f5bbe6dcb3e598df1b
Emmanuel Taiwo <100073682+devbird007@users.noreply.github.com>
2024-01-09 16:49:45 +0100

    x

--- TODO-CLI tool/cmd/todo/main.go
+++ TODO-CLI tool/cmd/todo/main.go
@@ -1,9 +1,9 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"os"
-	"strings"
 
 	"github.com/devbird007/interacting/todo"
 )
@@ -12,35 +12,55 @@ import (
 const todoFileName = ".todo.json"
 
 func main() {
+	// Parsing command line flags
+	task := flag.String("task", "", "Task to be included in the ToDo list")
+	list := flag.Bool("list", false, "List all tasks")
+	complete := flag.Int("complete", 0, "Item to be completed")
+	flag.Parse()
+
+	// Define an items list
 	l := &todo.List{}
 
-	// Use the Get method to read to do items from file
+	// Use the Get command to read to do items from file
 	if err := l.Get(todoFileName); err != nil {
 		fmt.Fprintln(os.Stderr, err)
 		os.Exit(1)
 	}
 
-	// Decide what to do based on the number of arguments provided
+	// Decide what to do based on the provided flags
 	switch {
-	// For no extra arguments, print the list
-	case len(os.Args) == 1:
+	case *list:
 		// List current to do items
 		for _, item := range *l {
-			fmt.Println(item.Task)
+			if !item.Done {
+				fmt.Println(item.Task)
+			}
 		}
-	// Concatenate all provided arguments with a space and
-	// add to the list as an item
-	default:
-		// Concatenate all arguments with a space
-		item := strings.Join(os.Args[1:], " ")
 
-		// Add the item
-		l.Add(item)
-
+	case *complete > 0:
+		// Complete the given item
+		if err := l.Complete(*complete); err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			os.Exit(1)
+		}
 		// Save the new list
 		if err := l.Save(todoFileName); err != nil {
 			fmt.Fprintln(os.Stderr, err)
 			os.Exit(1)
 		}
+
+	case *task != "":
+		// Add the task
+		l.Add(*task)
+
+		// Save the new list
+		if err := l.Save(todoFileName); err != nil {
+			fmt.Println(os.Stderr, err)
+			os.Exit(1)
+		}
+	default:
+		// Invalid flag provided
+		fmt.Fprintln(os.Stderr, "Invalid option")
+		os.Exit(1)
 	}
 }
