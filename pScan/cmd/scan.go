02ad67250af4babc10dc9373c45c002e6ade3ba3
Emmanuel Taiwo <100073682+devbird007@users.noreply.github.com>
2024-02-05 10:00:35 +0100

    updadded port range implementation

--- pScan/cmd/scan.go
+++ pScan/cmd/scan.go
@@ -6,9 +6,12 @@ Check LICENSE for details.
 package cmd
 
 import (
+	"errors"
 	"fmt"
 	"io"
 	"os"
+	"strconv"
+	"strings"
 
 	"github.com/devbird007/pScan/scan"
 	"github.com/spf13/cobra"
@@ -23,12 +26,26 @@ var scanCmd = &cobra.Command{
 	RunE: func(cmd *cobra.Command, args []string) error {
 		hostsFile := viper.GetString("hosts-file")
 
-		ports, err := cmd.Flags().GetIntSlice("ports")
+		portRange, err := cmd.Flags().GetString("port-range")
 		if err != nil {
 			return err
 		}
 
-		return scanAction(os.Stdout, hostsFile, ports)
+		if portRange == "" {
+			ports, err := cmd.Flags().GetIntSlice("ports")
+			if err != nil {
+				return err
+			}
+			return scanAction(os.Stdout, hostsFile, ports)
+
+		} else {
+			ports, err := parseString(portRange)
+			if err != nil {
+				return err
+			}
+
+			return scanAction(os.Stdout, hostsFile, ports)
+		}
 	},
 }
 
@@ -68,8 +85,34 @@ func printResults(out io.Writer, results []scan.Results) error {
 	return err
 }
 
+func parseString(s string) ([]int, error) {
+	parts := strings.Split(s, "-")
+
+	if len(parts) != 2 {
+		return nil, errors.New("invalid range format")
+	}
+
+	start, err := strconv.Atoi(parts[0])
+	if err != nil {
+		return nil, err
+	}
+
+	end, err := strconv.Atoi(parts[1])
+	if err != nil {
+		return nil, err
+	}
+
+	intSlice := make([]int, end-start+1)
+	for i := range intSlice {
+		intSlice[i] = start + i
+	}
+
+	return intSlice, nil
+}
+
 func init() {
 	rootCmd.AddCommand(scanCmd)
 
 	scanCmd.Flags().IntSliceP("ports", "p", []int{22, 88, 443}, "ports to scan")
+	scanCmd.Flags().StringP("port-range", "r", "", "range of ports to scan")
 }
