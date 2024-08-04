6470da77c01fc8e5e25c945d9b8dce4fe616edb3
Emmanuel Taiwo <100073682+devbird007@users.noreply.github.com>
2024-08-04 01:19:24 +0100

    Capture input from STDIN

--- new todo-cli tool/cmd/todo/main.go
+++ new todo-cli tool/cmd/todo/main.go
@@ -1,15 +1,18 @@
 package main
 
 import (
+	"bufio"
 	"flag"
 	"fmt"
+	"io"
 	"os"
+	"strings"
 
 	"github.com/devbird007/todo"
 )
 
-// Hardcoding the file name
-const todoFileName = ".todo.json"
+// Default file name
+var todoFileName = ".todo.json"
 
 func main() {
 	flag.Usage = func() {
@@ -21,11 +24,16 @@ func main() {
 	}
 
 	// Parsing command line flags
-	task := flag.String("task", "", "Task to be included in the ToDo list")
+	add := flag.Bool("add", false, "Add task to the ToDo list")
 	list := flag.Bool("list", false, "List all tasks")
 	complete := flag.Int("complete", 0, "Item to be completed")
 
 	flag.Parse()
+
+	// Check if the user defined the ENV VAR for a custom file name
+	if os.Getenv("TODO_FILENAME") != "" {
+		todoFileName = os.Getenv("TODO_FILENAME")
+	}
 
 	// Define an items list
 	l := &todo.List{}
@@ -55,9 +63,15 @@ func main() {
 			os.Exit(1)
 		}
 
-	case *task != "":
-		// Add the task
-		l.Add(*task)
+	case *add:
+		// When any arguments (excluding flags) are provided, they
+		// will be used as the new task
+		t, err := getTask(os.Stdin, flag.Args()...)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			os.Exit(1)
+		}
+		l.Add(t)
 
 		// Save the new list
 		if err := l.Save(todoFileName); err != nil {
@@ -70,4 +84,24 @@ func main() {
 		fmt.Fprintln(os.Stderr, "Invalid option")
 		os.Exit(1)
 	}
+}
+
+// getTask function decides where to get the description for a new
+// task from: arguments or STDIN
+func getTask(r io.Reader, args ...string) (string, error) {
+	if len(args) > 0 {
+		return strings.Join(args, " "), nil
+	}
+
+	s := bufio.NewScanner(r)
+	s.Scan()
+	if err := s.Err(); err != nil {
+		return "", err
+	}
+
+	if len(s.Text()) == 0 {
+		return "", fmt.Errorf("Task cannot be blank")
+	}
+
+	return s.Text(), nil
 }
